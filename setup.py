184d134ec26aebb285e308b39aeba636ff933f9d
alvarob96 <alvarob96@usal.es>
2018-12-12 14:58:58 +0100

    setup.py done

--- setup.py
+++ setup.py
@@ -9,5 +9,17 @@ setup(
     author='Álvaro Bartolomé',
     author_email='alvarob96@usal.es',
     description='This is a scrapping tool that retrieves continuous Spanish stock market information from https://es.investing.com, into a Pandas DataFrame.',
-    install_requires=['requests', 'pandas', 'beautifulsoup4', 'unidecode']
+    install_requires=['requests', 'pandas', 'beautifulsoup4', 'unidecode'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Topic :: Office/Business :: Financial",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+        "Topic :: Software Development :: Libraries",
+        "Operating System :: OS Independent",
+    ],
+    keywords='investing, scrapper, pandas'
 )
