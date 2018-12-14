f6fa8af807c2618c072c02dfeef0d33329603808
Álvaro Bartolomé <alvarob96@usal.es>
2018-12-14 10:41:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='investing_scrapper',
-    version='0.2.5',
+    version='0.2.6',
     packages=find_packages(),
     url='',
-    download_url='https://github.com/alvarob96/investing-scrapper/archive/0.2.5.tar.gz',
+    download_url='https://github.com/alvarob96/investing-scrapper/archive/0.2.6.tar.gz',
     license='MIT License',
     author='Álvaro Bartolomé',
     author_email='alvarob96@usal.es',
     description='This is a scrapping tool that retrieves continuous Spanish stock market information from https://es.investing.com, into a Pandas DataFrame.',
     install_requires=['requests', 'pandas', 'beautifulsoup4'],
-    data_files=[('tickers', ['data/tickers.csv']), ('user-agents', ['data/user-agent-list.txt'])],
+    data_files=[
+        ('tickers', ['investing_scrapper/resources/tickers.csv']),
+        ('user-agents', ['investing_scrapper/resources/user-agent-list.txt'])
+    ],
     include_package_data=True,
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
