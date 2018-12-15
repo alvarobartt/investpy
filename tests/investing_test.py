623be478570d7fed1a91209e4a3861a7621ad6e1
alvarob96 <alvarob96@usal.es>
2018-12-15 17:59:50 +0100

    pytest done & new travis

--- tests/investing_test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import pytest
-
-import investing_scrapper
-
-investing_scrapper.get_recent_data(ticker='bbva')
-investing_scrapper.get_historical_data(ticker='bbva', start='10/10/2018', end='10/12/2018')
