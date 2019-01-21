fa5783b811d696f4e0034e93fb6f1c01e152dada
alvarob96 <alvarob96@usal.es>
2019-01-21 14:32:49 +0100

    equities extra stock information

--- investing_scrapper/Data.py
+++ investing_scrapper/Data.py
@@ -2,14 +2,20 @@
 
 
 class Data(object):
-    def __init__(self, date_, close_, open_):
+    def __init__(self, date_, close_, open_, max_, min_, volume_):
         self.date = date_
         self.close = close_
         self.open = open_
+        self.max = max_
+        self.min = min_
+        self.volume = volume_
 
     def to_dict(self):
         return {
             'Date': self.date,
             'Close': self.close,
             'Open': self.open,
+            'Max': self.max,
+            'Min': self.min,
+            'Volume': self.volume,
         }
--- investing_scrapper/__init__.py
+++ investing_scrapper/__init__.py
@@ -8,7 +8,7 @@ import requests
 from bs4 import BeautifulSoup
 
 from investing_scrapper.Data import Data # TypeError: 'module' object is not callable
-from investing_scrapper import user_agent as ua, equities as ts
+from investing_scrapper import user_agent as ua, equities as ts, funds as fs
 
 
 def get_recent_data(equity):
@@ -22,7 +22,7 @@ def get_recent_data(equity):
 
     for row in equities.itertuples():
         if row.name.lower() == equity.lower():
-            url = "https://es.investing.com/equities/" + equity + "-historical-data"
+            url = "https://es.investing.com/equities/" + row.tag + "-historical-data"
             headers = {
                 'User-Agent': ua.get_random()
             }
@@ -37,11 +37,23 @@ def get_recent_data(equity):
             result = list()
 
             for element in selection:
-                info = element.getText().strip().split('\n')
+                info = element.getText().strip().split()
+
                 stock_date = datetime.datetime.strptime(info[0].replace('.', '-'), '%d-%m-%Y')
-                stock_close = int(info[1].replace(',', ''))
-                stock_open = int(info[2].replace(',', ''))
-                result.insert(len(result), Data(stock_date, stock_close, stock_open))
+                stock_close = float(info[1].replace(',', '.'))
+                stock_open = float(info[2].replace(',', '.'))
+                stock_max = float(info[3].replace(',', '.'))
+                stock_min = float(info[4].replace(',', '.'))
+                stock_volume = 0
+
+                if info[5].__contains__('K'):
+                    stock_volume = int(float(info[5].replace('K', '').replace(',', '.')) * 1000)
+                elif info[5].__contains__('M'):
+                    stock_volume = int(float(info[5].replace('M', '').replace(',', '.')) * 1000000)
+                elif info[5].__contains__('B'):
+                    stock_volume = int(float(info[5].replace('B', '').replace(',', '.')) * 1000000000)
+
+                result.insert(len(result), Data(stock_date, stock_close, stock_open, stock_max, stock_min, stock_volume,))
 
             result = result[::-1]
 
@@ -52,12 +64,34 @@ def get_recent_data(equity):
 
 
 def get_historical_data(equity, start, end):
-    for item in ts.get_equity_names():
-        if item['name'].lower() == equity.lower():
+    resource_package = __name__
+    resource_path = '/'.join(('resources', 'equities.csv'))
+    if pkg_resources.resource_exists(resource_package, resource_path):
+        equities = pd.read_csv(pkg_resources.resource_filename(resource_package, resource_path))
+    else:
+        names = ts.get_equity_names()
+        equities = pd.DataFrame(names)
+
+    for row in equities.itertuples():
+        if row.name.lower() == equity.lower():
+            url = "https://es.investing.com/equities/" + row.tag + "-historical-data"
+            headers = {
+                'User-Agent': ua.get_random()
+            }
+
+            req = requests.get(url, headers=headers)
+
+            status = req.status_code
+            html = BeautifulSoup(req.text, 'html.parser')
+
+            selection = html.select('div.instrumentHeader > h2.float_lang_base_1')
+            for element in selection:
+                header = element.text
+
             params = {
                 "curr_id": "558",
                 "smlID": "1159685",
-                "header": "Datos históricos SAN",
+                "header": header,
                 "st_date": start,
                 "end_date": end,
                 "interval_sec": "Daily",
@@ -82,11 +116,24 @@ def get_historical_data(equity, start, end):
             result = list()
 
             for element in selection:
-                info = element.getText().strip().split('\n')
+                info = element.getText().strip().split()
+                print(info)
+
                 stock_date = datetime.datetime.strptime(info[0].replace('.', '-'), '%d-%m-%Y')
-                stock_close = int(info[1].replace(',', ''))
-                stock_open = int(info[2].replace(',', ''))
-                result.insert(len(result), Data(stock_date, stock_close, stock_open))
+                stock_close = float(info[1].replace(',', '.'))
+                stock_open = float(info[2].replace(',', '.'))
+                stock_max = float(info[3].replace(',', '.'))
+                stock_min = float(info[4].replace(',', '.'))
+                stock_volume = 0
+
+                if info[5].__contains__('K'):
+                    stock_volume = int(float(info[5].replace('K', '').replace(',', '.')) * 1000)
+                elif info[5].__contains__('M'):
+                    stock_volume = int(float(info[5].replace('M', '').replace(',', '.')) * 1000000)
+                elif info[5].__contains__('B'):
+                    stock_volume = int(float(info[5].replace('B', '').replace(',', '.')) * 1000000000)
+
+                result.insert(len(result), Data(stock_date, stock_close, stock_open, stock_max, stock_min, stock_volume,))
 
             result = result[::-1]
 
@@ -94,3 +141,53 @@ def get_historical_data(equity, start, end):
             df.set_index('Date', inplace=True)
 
             return df
+
+
+"""def get_fund_recent_data(fund):
+    resource_package = __name__
+    resource_path = '/'.join(('resources', 'funds.csv'))
+    if pkg_resources.resource_exists(resource_package, resource_path):
+        funds = pd.read_csv(pkg_resources.resource_filename(resource_package, resource_path))
+    else:
+        names = fs.get_fund_names()
+        funds = pd.DataFrame(names)
+
+    for row in funds.itertuples():
+        if row.name.lower() == fund.lower():
+            url = "https://es.investing.com/funds/" + row.tag + "-historical-data"
+            headers = {
+                'User-Agent': ua.get_random()
+            }
+
+            req = requests.get(url, headers=headers)
+
+            status = req.status_code
+            html = BeautifulSoup(req.text, 'html.parser')
+
+            selection = html.select('div#results_box > table#curr_table > tbody > tr')
+
+            result = list()
+
+            for element in selection:
+                info = element.getText().strip().split()
+
+                stock_date = datetime.datetime.strptime(info[0].replace('.', '-'), '%d-%m-%Y')
+                stock_close = float(info[1].replace(',', '.'))
+                stock_open = float(info[2].replace(',', '.'))
+                stock_max = float(info[3].replace(',', '.'))
+                stock_min = float(info[4].replace(',', '.'))
+                stock_volume = 0
+
+                if info[5].__contains__('M'):
+                    stock_volume = int(float(info[5].replace('M', '').replace(',', '.')) * 1000000)
+                elif info[5].__contains__('B'):
+                    stock_volume = int(float(info[5].replace('B', '').replace(',', '.')) * 1000000000)
+
+                result.insert(len(result), Data(stock_date, stock_close, stock_open, stock_max, stock_min, stock_volume,))
+
+            result = result[::-1]
+
+            df = pd.DataFrame.from_records([value.to_dict() for value in result])
+            df.set_index('Date', inplace=True)
+
+            return df"""
