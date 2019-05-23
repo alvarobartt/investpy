2a8653b1b3104f264ebe792907448a4c4f7fd674
alvarob96 <alvarob96@usal.es>
2019-05-23 20:07:16 +0200

    codecov fix #3

--- tests/test_investpy.py
+++ tests/test_investpy.py
@@ -19,26 +19,35 @@ def test_investing():
 
     investpy.get_equities()
     investpy.get_equities_list()
-    investpy.get_recent_data(equity='enagás', as_json=True, order='ascending')
-    investpy.get_historical_data(equity='enagás', start='30/10/2018', end='30/12/2018', as_json=False, order='ascending')
-    investpy.get_equity_company_profile(equity='enagás', language='spanish')
+
+    for value in [True, False]:
+        investpy.get_recent_data(equity='enagás', as_json=value, order='ascending')
+        investpy.get_historical_data(equity='enagás', start='01/01/1990', end='01/01/2019', as_json=value, order='ascending')
+
+    for value in ['spanish', 'language']:
+        investpy.get_equity_company_profile(equity='enagás', language=value)
 
     get_equity_names()
 
     investpy.get_funds()
     investpy.get_funds_list()
-    investpy.get_funds_dict(columns=['id', 'tag'], as_json=False)
-    investpy.get_fund_recent_data(fund='bbva multiactivo conservador pp', as_json=False, order='ascending')
-    investpy.get_fund_historical_data(fund='bbva multiactivo conservador pp', start='30/10/2018', end='30/12/2018', as_json=False, order='ascending')
+
+    for value in [True, False]:
+        investpy.get_funds_dict(columns=['id', 'tag'], as_json=value)
+        investpy.get_fund_recent_data(fund='bbva multiactivo conservador pp', as_json=value, order='ascending')
+        investpy.get_fund_historical_data(fund='bbva multiactivo conservador pp', start='01/01/1990', end='01/01/2019', as_json=value, order='ascending')
+
     investpy.get_fund_information(fund='bbva multiactivo conservador pp')
 
     get_fund_names()
 
     investpy.get_etfs()
     investpy.get_etfs_list()
-    investpy.get_etfs_dict(columns=['id', 'name'], as_json=False)
-    investpy.get_etf_recent_data(etf='bbva accion dj eurostoxx 50', as_json=False, order='ascending')
-    investpy.get_etf_historical_data(etf='bbva accion dj eurostoxx 50', start='30/10/2018', end='30/12/2018', as_json=False, order='ascending')
+
+    for value in [True, False]:
+        investpy.get_etfs_dict(columns=['id', 'name'], as_json=value)
+        investpy.get_etf_recent_data(etf='bbva accion dj eurostoxx 50', as_json=value, order='ascending')
+        investpy.get_etf_historical_data(etf='bbva accion dj eurostoxx 50', start='01/01/1990', end='01/01/2019', as_json=value, order='ascending')
 
     get_etf_names()
 
