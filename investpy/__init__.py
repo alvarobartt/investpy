1cfc35c8f528bdd18c48532847d9e0d3291deb48
Hassan @Ubuntu <hassansalehe@gmail.com>
2021-03-20 22:58:39 +0100

    Remove everything except stocks and currency cross

--- investpy/__init__.py
+++ investpy/__init__.py
@@ -8,32 +8,9 @@ from .stocks import get_stocks, get_stocks_list, get_stocks_dict, get_stock_coun
     get_stock_historical_data, get_stock_company_profile, get_stock_dividends, get_stock_information, get_stocks_overview, \
     get_stock_financial_summary, search_stocks
 
-from .funds import get_funds, get_funds_list, get_funds_dict, get_fund_countries, get_fund_recent_data, \
-    get_fund_historical_data, get_fund_information, get_funds_overview, search_funds
-
-from .etfs import get_etfs, get_etfs_list, get_etfs_dict, get_etf_countries, get_etf_recent_data, \
-    get_etf_historical_data, get_etf_information, get_etfs_overview, search_etfs
-
-from .indices import get_indices, get_indices_list, get_indices_dict, get_index_countries, \
-    get_index_recent_data, get_index_historical_data, get_index_information, get_indices_overview, search_indices
-
 from .currency_crosses import get_currency_crosses, get_currency_crosses_list, get_currency_crosses_dict, \
     get_available_currencies, get_currency_cross_recent_data, get_currency_cross_historical_data, \
     get_currency_cross_information, get_currency_crosses_overview, search_currency_crosses
-
-from .bonds import get_bonds, get_bonds_list, get_bonds_dict, get_bond_countries, get_bond_recent_data, \
-    get_bond_historical_data, get_bond_information, get_bonds_overview, search_bonds
-
-from .commodities import get_commodities, get_commodities_list, get_commodities_dict, get_commodity_groups, \
-    get_commodity_recent_data, get_commodity_historical_data, get_commodity_information, get_commodities_overview, \
-    search_commodities
-
-from .crypto import get_cryptos, get_cryptos_list, get_cryptos_dict, get_crypto_recent_data, \
-    get_crypto_historical_data, get_crypto_information, get_cryptos_overview, search_cryptos
-
-from .certificates import get_certificates, get_certificates_list, get_certificates_dict, get_certificate_countries, \
-    get_certificate_recent_data, get_certificate_historical_data, get_certificate_information, get_certificates_overview, \
-    search_certificates
 
 from .search import search_quotes
 
