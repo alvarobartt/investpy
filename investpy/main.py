93e3c7c8830644b59e4fe8af53477361949a5fd8
BCraine <brandoncraine193@gmail.com>
2021-04-05 18:33:39 -0400

    Added main .py file with working get_stock_financial_summary function

---
+++ investpy/main.py
@@ -0,0 +1,12 @@
+import investpy
+
+
+def main():
+    data = investpy.get_stock_financial_summary(stock='AAPL', country='United States', summary_type='income_statement',
+                                                period='annual')
+    print(data.head())
+
+
+if __name__ == '__main__':
+    main()
+
