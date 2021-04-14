39e9b9dcd5486d29deefdd351bd3201188e30ac4
BCraine <brandoncraine193@gmail.com>
2021-04-14 00:21:12 -0400

    correct index for Dow Jones is now used

--- investpy/main.py
+++ investpy/main.py
@@ -18,13 +18,25 @@ def main():
 
     # data = investpy.technical_indicators(name='bbva', country='spain', product_type='stock', interval='daily')
     # data = investpy.technical_indicators(name='aapl', country='united states', product_type='stock', interval='daily')
-    data = investpy.technical_indicators(name='gme', country='united states', product_type='stock', interval='daily')
+    # data = investpy.technical_indicators(name='gme', country='united states', product_type='stock', interval='daily')
     # data = investpy.technical_indicators(name='U.S. 10Y', country='united states', product_type='bond', interval='daily')
     # data = investpy.technical_indicators(name='gme', country='united states', product_type='stock', interval='weekly')
 
-    print(data)
+    # print(data)
+
+    data = investpy.get_index_historical_data(index='DJ US',
+                                              country='united states',
+                                              interval='Daily',
+                                              from_date='01/01/1996',
+                                              to_date='17/12/2020',
+                                              order='descending',
+                                              as_json=True)
+    print(data.head())
+
+    # data = investpy.get_indices_list(country='United States')
+    # print(data)
+
 
 
 if __name__ == '__main__':
     main()
-
